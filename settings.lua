b8d9832463cc3eb601117743270539c8626bb4c9
D4KiR <d4kir92@gmail.com>
2023-06-21 09:38:35 +0200

    v0.2.29

--- settings.lua
+++ settings.lua
@@ -1,7 +1,7 @@
 local _, DarkMode = ...
 
 local config = {
-	["title"] = format("DarkMode |T136122:16:16:0:0|t v|cff3FC7EB%s", "0.2.28")
+	["title"] = format("DarkMode |T136122:16:16:0:0|t v|cff3FC7EB%s", "0.2.29")
 }
 
 local searchStr = ""
