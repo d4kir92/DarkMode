0fbaab52c1f9aa845ff7c969dbeb8f9edd5dfe55
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-06-22 11:07:04 +0200

    v0.5.58

--- settings.lua
+++ settings.lua
@@ -289,8 +289,8 @@ function DarkMode:InitDMSettings()
 		DMSettings:Hide()
 	end
 
-	DarkMode:SetVersion(AddonName, 136122, "0.5.57")
-	DMSettings.TitleText:SetText(format("DarkMode |T136122:16:16:0:0|t v|cff3FC7EB%s", "0.5.57"))
+	DarkMode:SetVersion(AddonName, 136122, "0.5.58")
+	DMSettings.TitleText:SetText(format("DarkMode |T136122:16:16:0:0|t v|cff3FC7EB%s", "0.5.58"))
 	DMSettings.CloseButton:SetScript(
 		"OnClick",
 		function()
