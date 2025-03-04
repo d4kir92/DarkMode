c674f0a790aac9024246a2fd636a450b04744ad3
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-03-04 09:11:35 +0100

    v0.6.13

--- settings.lua
+++ settings.lua
@@ -344,7 +344,7 @@ function DarkMode:ToggleSettings()
 end
 
 function DarkMode:InitDMSettings()
-	DarkMode:SetVersion(136122, "0.6.12")
+	DarkMode:SetVersion(136122, "0.6.13")
 	if not DarkMode:IsOldWow() then
 		DMSettings = CreateFrame("Frame", "DMSettings", UIParent, "BasicFrameTemplate")
 	else
