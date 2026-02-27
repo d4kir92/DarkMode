3e0f0e4b4b00866f6118192476ff4ba4b5d5ffdd
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-02-27 12:59:37 +0100

    v0.7.112
    TransmogFrame

--- settings.lua
+++ settings.lua
@@ -263,7 +263,7 @@ function DarkMode:AddColor(px, key, value, cKey, add)
 end
 
 function DarkMode:InitDMSettings()
-	DarkMode:SetVersion(136122, "0.7.111")
+	DarkMode:SetVersion(136122, "0.7.112")
 	if not DarkMode:IsOldWow() then
 		DMSettings = DarkMode:CreateFrame("DMSettings", UIParent, "BasicFrameTemplate")
 	else
