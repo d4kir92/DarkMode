a7635b4a1e67aeed576d5f2f6dbb5716b7abe76a
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-07-23 08:26:22 +0200

    v0.7.144

--- settings.lua
+++ settings.lua
@@ -263,7 +263,7 @@ function DarkMode:AddColor(px, key, value, cKey, add)
 end
 
 function DarkMode:InitDMSettings()
-	DarkMode:SetVersion(136122, "0.7.143")
+	DarkMode:SetVersion(136122, "0.7.144")
 	if not DarkMode:IsOldWow() then
 		DMSettings = DarkMode:CreateFrame("DMSettings", UIParent, "BasicFrameTemplate")
 	else
