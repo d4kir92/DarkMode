2a987a017687aa63acd4768d52d81e348105d3f5
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-09-02 09:00:22 +0200

    v0.8.2

--- settings.lua
+++ settings.lua
@@ -358,7 +358,7 @@ function DarkMode:ToggleSettings()
 end
 
 function DarkMode:InitDMSettings()
-	DarkMode:SetVersion(136122, "0.8.1")
+	DarkMode:SetVersion(136122, "0.8.2")
 	dmSettings = DarkMode:CreateUIWindow({
 		["name"] = "DMSettings",
 		["pTab"] = {"CENTER"},
